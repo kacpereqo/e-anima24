acf3e2c7837105eb3640483f927e076c89e0ab7f
kacpereqqo <remzak.k@gmail.com>
2023-06-30 22:17:48 +0200

    idk

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,14 +1,24 @@
 <template>
   <div class="index-page">
     <TheFeaturedAnime />
-    <div class="series"></div>
+    <div class="anime-showcase-list">
+      <AnimeShowCase category="popular" />
+      <AnimeShowCase category="trending" />
+      <AnimeShowCase category="new" />
+    </div>
   </div>
 </template>
 
 <style scoped>
-#index-page {
+.index-page {
   display: flex;
   flex-direction: column;
-  gap: 2rem;
+  gap: 3rem;
+}
+
+.anime-showcase-list {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
 }
 </style>
